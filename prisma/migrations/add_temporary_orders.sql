49d3a1de3d4cd6caafcc90eaaa661827e88c60f3
rsetbrasil <rsetbrasil@gmail.com>
2026-05-23 15:08:45 -0300

    fix: migrar Prisma de MySQL para PostgreSQL e criar tabelas no start

--- prisma/migrations/add_temporary_orders.sql
+++
@@ -1,9 +0,0 @@
-
--- CreateTable
-CREATE TABLE `temporary_orders_data` (
-    `id` VARCHAR(191) NOT NULL,
-    `data` JSON NOT NULL,
-    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
